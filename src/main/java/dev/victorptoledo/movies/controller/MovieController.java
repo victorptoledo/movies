0966b4f08a29efc502b1c9e36889376bb867061d
Victor <victorptoledo@outlook.com>
2023-08-05 11:56:23 -0300

    Changing getAllMovies to return a List of Movies

--- src/main/java/dev/victorptoledo/movies/controller/MovieController.java
+++ src/main/java/dev/victorptoledo/movies/controller/MovieController.java
@@ -1,16 +1,27 @@
 package dev.victorptoledo.movies.controller;
 
+import dev.victorptoledo.movies.domain.Movie;
+import dev.victorptoledo.movies.service.MovieService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService service) {
+        this.movieService = service;
+    }
+
     @GetMapping
-    public ResponseEntity<String> getAllMovies() {
-        return new ResponseEntity<String>("All Movies!", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.AllMovies(), HttpStatus.OK);
     }
 }
