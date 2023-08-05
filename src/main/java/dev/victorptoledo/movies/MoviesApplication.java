3f0b703bbb3480c79afadca13a6338ccffb6e516
Victor <victorptoledo@outlook.com>
2023-08-05 10:25:10 -0300

    Initial Commit

---
+++ src/main/java/dev/victorptoledo/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package dev.victorptoledo.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
