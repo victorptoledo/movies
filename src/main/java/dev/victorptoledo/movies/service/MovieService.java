3edfc94aa699dcf19a4a9b558436832cec7366e5
Victor <victorptoledo@outlook.com>
2023-08-05 11:49:45 -0300

    Create Movie Service

---
+++ src/main/java/dev/victorptoledo/movies/service/MovieService.java
@@ -0,0 +1,21 @@
+package dev.victorptoledo.movies.service;
+
+import dev.victorptoledo.movies.domain.Movie;
+import dev.victorptoledo.movies.repository.MovieRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+
+    private MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public List<Movie> AllMovies(){
+        return movieRepository.findAll();
+    }
+}
