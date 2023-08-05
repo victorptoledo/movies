5165d559b95bcb9aa4fd51a7ea97cd8ab2d3bda7
Victor <victorptoledo@outlook.com>
2023-08-05 12:52:06 -0300

    Creating the review section

---
+++ src/main/java/dev/victorptoledo/movies/repository/ReviewRepository.java
@@ -0,0 +1,11 @@
+package dev.victorptoledo.movies.repository;
+
+import dev.victorptoledo.movies.domain.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
---
+++ src/main/java/dev/victorptoledo/movies/service/ReviewService.java
@@ -0,0 +1,30 @@
+package dev.victorptoledo.movies.service;
+
+import dev.victorptoledo.movies.domain.Movie;
+import dev.victorptoledo.movies.domain.Review;
+import dev.victorptoledo.movies.repository.ReviewRepository;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    private final MongoTemplate mongoTemplate;
+
+    public ReviewService(ReviewRepository reviewRepository, MongoTemplate mongoTemplate) {
+        this.reviewRepository = reviewRepository;
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = new Review(reviewBody);
+        reviewRepository.insert(review);
+        mongoTemplate.update(Movie.class).matching(Criteria.where("imdbId").is(imdbId)).apply(new Update().push(
+                "reviewIds").value(review)).first();
+        return review;
+    }
+}
