c71d9c87ba0230ca8c0b00a2bd4641e59a5cd245
Victor <victorptoledo@outlook.com>
2023-08-05 13:02:15 -0300

    Creating the review controller

---
+++ src/main/java/dev/victorptoledo/movies/controller/ReviewController.java
@@ -0,0 +1,29 @@
+package dev.victorptoledo.movies.controller;
+
+import dev.victorptoledo.movies.domain.Review;
+import dev.victorptoledo.movies.service.ReviewService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")),
+                HttpStatus.CREATED);
+    }
+}
